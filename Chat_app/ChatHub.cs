7d52e5c9dbf3430a0a41cdf792361320068a5bb2
TATREQUE <abdullahtarequepi@gmail.com>
2023-10-10 23:04:04 +0600

    Add project files.

---
+++ Chat_app/ChatHub.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Chat_app
+{
+    public class ChatHub : Hub
+    {
+        public void Send(string name, string message)
+        {
+            Clients.All.BroadcastMessage(name, message);
+        }
+
+        public void ShareFile(string name, string fileName, string fileData)
+        {
+            Clients.All.BroadcastFile(name, fileName, fileData);
+        }
+    }
+}
